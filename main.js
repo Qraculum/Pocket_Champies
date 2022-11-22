e6b280a039849f2060b09f1d31206fef7dd9b56d
Qraculum <alexander.unive@gmail.com>
2022-11-23 00:10:54 +0100

    save

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 let body = document.querySelector("body")
-body.innerHTML = `<img id="champie" src="Rek'Sai/Sprite_readjusted - Idle2.png">` + body.innerHTML
+document.getElementById("champie_box").innerHTML = `<img id="champie" height="256" src="Rek'Sai/Sprite_readjusted - Idle2.png">`
 
 let champie = document.querySelector("#champie")
-    //rotate "champie" 360deg, after 0.2s modify transition duration to 0s (to disallow next animation) and remove transform, return back the 0.2s to transition duration
-    champie.addEventListener("click", (event) => {
-        champie.style.transform = "rotateY(360deg)"
-        setTimeout(event = () => {champie.style.transitionDuration = "0s"; champie.style.removeProperty("transform")}, 200)
-        champie.style.transitionDuration = "0.2s"
-    })
-let idleSprite = 1, pos
+let idleSprite = 1, pos, key, i
 
 
 
@@ -22,6 +16,36 @@ let idleAnimStart = setInterval(IdleAnimation, 500)
 
 
 //rearrange champie's position on scene
-function ChangePosition() {champie.style.top = `${document.querySelector("#background").clientHeight - champie.clientHeight}px`}
+function ChangePosition() {document.querySelector("#champie_box").style.marginTop = `${document.querySelector("#background").clientHeight - champie.clientHeight}px`}
 ChangePosition()
 console.log(champie.clientHeight)
+
+
+
+//CHAMPIE 
+//rotate "champie" 360deg, after 0.2s modify transition duration to 0s (to disallow next animation) and remove transform, return back the 0.2s to transition duration
+champie.addEventListener("click", (event) => {
+    champie.style.transform = "rotateY(360deg)"
+    setTimeout(event = () => {champie.style.transitionDuration = "0s"; champie.style.removeProperty("transform")}, 200)
+    champie.style.transitionDuration = "0.2s"
+})
+
+document.addEventListener("keydown", (event) => {
+    key = event.keyCode
+    if (key == `69`) {
+        i = 1
+        function Repeat() {
+            setTimeout(event = () => {
+                y = Math.log2(i+25)*75-350 
+                console.log(y)
+
+                champie.style.left = `${i}px` 
+                champie.style.bottom = `${y}px`
+
+                i++
+                if(i <= 516) Repeat()
+            }, 5)
+        }
+        Repeat()
+    }
+})
