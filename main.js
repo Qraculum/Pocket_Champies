1f635adb365b171b740d9ef55280a6995a92f2f4
Qraculum <alexander.unive@gmail.com>
2022-11-22 22:06:59 +0100

    save

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 let body = document.querySelector("body")
 body.innerHTML = `<img id="champie" src="Rek'Sai/Sprite_readjusted - Idle2.png">` + body.innerHTML
 
-let champie = document.querySelector("#champie"); champie.addEventListener("click", (event) => {champie.style.transform = "rotateY(360deg)"})
+let champie = document.querySelector("#champie")
+    //rotate "champie" 360deg, after 0.2s modify transition duration to 0s (to disallow next animation) and remove transform, return back the 0.2s to transition duration
+    champie.addEventListener("click", (event) => {
+        champie.style.transform = "rotateY(360deg)"
+        setTimeout(event = () => {champie.style.transitionDuration = "0s"; champie.style.removeProperty("transform")}, 200)
+        champie.style.transitionDuration = "0.2s"
+    })
 let idleSprite = 1, pos
 
 
