86816dd7a6fa40b7e30d2c8d1c87a5465fab9b41
Qraculum <alexander.unive@gmail.com>
2022-11-26 21:36:09 +0100

    Added images, fonts and titles to boxes

--- champieSkills.js
+++ champieSkills.js
@@ -1,6 +1,6 @@
 //TODO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MISCELLANEOUS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-//? rearrange champie's position on scene (creates margin-top to put object on the ground)
+//? rearrange champie's position on scene (creates margin-top to put the object on the ground)
 function ChangePosition() {
     document.querySelector("#champie_box").style.marginTop = `${document.querySelector("#background").clientHeight - champie.clientHeight}px`
 }
@@ -9,28 +9,34 @@ function ChangePosition() {
 //TODO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~REK'SAI~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 //! --------------------------------------ATTACK
-reksaiChampie.Attack = function (x) {  
+reksaiChampie.Attack = function () {  
+    x = 2
     function AttackP1() {                                   //? PHASE 1
-        setTimeout(event = () => {  // moving with a little delay (time)
+        setTimeout(event = () => {  // move with a little delay (time)
             champie.style.left = `${-x}px`  // every iteration decrease x to move further to the left   
     
             x++  // lower = slower | higher = faster
-            if(x%10 == 0) time += 2  // slowing down the backwards charge
-            if(x <= 90) AttackP1()  // looping the same phase to iterate the movement
-            else {x=3; time=1; x_old=x; AttackP2()}  // if x>90 then starts a new phase and adds this.x to the new phase 
+            if(x%10 == 0) time += 2  // slow down the backwards charge
+            if(x <= 90) AttackP1()  // loop the same phase to iterate the movement
+            else {x=3; time=1; x_old=x; AttackP2()}  // if x>90 then start new phase and adds this.x to the new phase 
         }, time)
     }
     function AttackP2() {                                   //? PHASE 2
-        setTimeout(event = () => {  // moving with a little delay (time)
-            y = Math.sin(x/150)*50  // every iteration creates new y value based on function    
+        setTimeout(event = () => {  // move with a little delay (time)
+            y = Math.sin(x/150)*50  // every iteration create new y value based on function    
     
-            champie.style.left = `${x}px`  // moves champie
+            champie.style.left = `${x}px`  // move champie
             champie.style.bottom = `${y}px`
     
             x += 3.5  // lower = slower | higher = faster
-            if(x <= 516) AttackP2()  // looping the same phase to iterate the movement
+            if(x <= 516) AttackP2()  // loop the same phase to iterate the movement
             else setTimeout(event = () => {champie.style.removeProperty("left"); champie.style.removeProperty("bottom")}, 500)  //after x time resets the champie's position
         }, 1)
     }
     AttackP1()  //? first function call
+}
+
+//! --------------------------------------BLOCK
+reksaiChampie.Block = function () {  
+
 }
--- main.js
+++ main.js
@@ -8,32 +8,36 @@ console.log(reksaiChampie)
 
 
 //TODO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SELECTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-let /*tab = [0, 0, 0, 0, 0], boxIDTab = ["a", "b", "reksai", "c", "d"],*/ wait = true
-document.querySelectorAll(".beginning_box").forEach(beginning_box => {  // add transition to every .beginning_box
-    beginning_box.style.transition = "all 0.4s ease-out"
+let wait = true
+document.querySelectorAll(".champie_box").forEach(champie_box => {  // add transition to every .champie_box
+    champie_box.style.transition = "all 0.4s ease-out"
 })
 
-//? confirm and select the middle available champie
+//? confirm, select the middle available champie and call the Start() function
 function Confirm() {
-        // get id from centerized beginning_box
-        selectedChampie = document.getElementsByClassName("beginning_box")[2].id
-        if (selectedChampie == "reksaiChampie") selectedChampie = reksaiChampie  //? declaring selectedChampie as x pokemon' object
+        // get id from centerized champie_box
+        selectedChampie = document.getElementsByClassName("champie_box")[2].id
+        if (selectedChampie == "reksaiChampie") selectedChampie = reksaiChampie  // declaring selectedChampie as x pokemon' object
 
         Start()
 }
 
 //? swipe all champies to the left
-function ToggleLeft() {
+function ToggleRight() {
     if (wait) {
-        for(i=0; i<5; i++) document.getElementsByClassName("beginning_box")[i].style.transform = "translate(-300px, 0px)"
+        // move all boxes to the left by 300px
+        for(i=0; i<5; i++) document.getElementsByClassName("champie_box")[i].style.transform = "translate(-300px, 0px)"  
 
+        // after 0.4s 
         setTimeout(event => {
-            document.querySelector("#pokemon_selection_case").appendChild(document.getElementsByClassName("beginning_box")[0], document.getElementsByClassName("beginning_box")[4])
-            document.querySelectorAll(".beginning_box").forEach(beginning_box => {
-                beginning_box.style.removeProperty("transition")
-                beginning_box.style.removeProperty("transform")
+            // put the first object as the last
+            document.querySelector("#pokemon_selection_case").appendChild(document.getElementsByClassName("champie_box")[0], document.getElementsByClassName("champie_box")[4])
+            document.querySelectorAll(".champie_box").forEach(champie_box => {  // for every champie_box remove transition and transform in style
+                champie_box.style.removeProperty("transition")
+                champie_box.style.removeProperty("transform")
             })
-            setTimeout(event => {document.querySelectorAll(".beginning_box").forEach(beginning_box => { beginning_box.style.transition = "all 0.4s ease-out" })}, 400)
+            // after 0.4s add transition to every box
+            setTimeout(event => {document.querySelectorAll(".champie_box").forEach(champie_box => { champie_box.style.transition = "all 0.4s ease-out" })}, 400)
         }, 400)
 
         wait = false
@@ -42,26 +46,21 @@ function ToggleLeft() {
 }
 
 //? swipe all champies to the right
-function ToggleRight() {
+function ToggleLeft() {
     if (wait) {
-        for(i=0; i<5; i++) document.getElementsByClassName("beginning_box")[i].style.transform = "translate(300px, 0px)"
+        // move all boxes to the right by 300px
+        for(i=0; i<5; i++) document.getElementsByClassName("champie_box")[i].style.transform = "translate(300px, 0px)"
 
+        // after 0.4s 
         setTimeout(event => {
-                /*
-                for(i=0; i<5; i++){
-                    tab[i+1] = boxIDTab[i]
-                }
-                tab.pop()
-                boxIDTab = tab
-                tab = [0, 0, 0, 0, 0]
-                console.log(boxIDTab)
-                */
-            document.querySelector("#pokemon_selection_case").insertBefore(document.getElementsByClassName("beginning_box")[4], document.getElementsByClassName("beginning_box")[0])
-            document.querySelectorAll(".beginning_box").forEach(beginning_box => {
-                beginning_box.style.removeProperty("transition")
-                beginning_box.style.removeProperty("transform")
+            // put the last object as the first
+            document.querySelector("#pokemon_selection_case").insertBefore(document.getElementsByClassName("champie_box")[4], document.getElementsByClassName("champie_box")[0])
+            document.querySelectorAll(".champie_box").forEach(champie_box => {
+                champie_box.style.removeProperty("transition")
+                champie_box.style.removeProperty("transform")
             })
-            setTimeout(event => {document.querySelectorAll(".beginning_box").forEach(beginning_box => { beginning_box.style.transition = "all 0.4s ease-out" })}, 400)
+            // after 0.4s add transition to every box
+            setTimeout(event => {document.querySelectorAll(".champie_box").forEach(champie_box => { champie_box.style.transition = "all 0.4s ease-out" })}, 400)
         }, 400)
 
         wait = false
@@ -70,18 +69,18 @@ function ToggleRight() {
 }
 
 //TODO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~GAME~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//! starts the game and chooses the pokemon
+//? starts the game
 function Start() {
-    //? remove whole selection menu
+    // remove whole selection menu
     document.querySelector("#selection_background").parentNode.removeChild(document.querySelector("#selection_background")) 
 
-    //? create viable character in champie_box
+    // create viable character in champie_box
     document.getElementById("champie_box").innerHTML = `<img id="champie" height="${selectedChampie.height}" src="${selectedChampie.name}/Sprite_readjusted - Idle2.png">` 
     let champie = document.getElementById("champie")  // declaring champie's variable
     ChangePosition()  // rearrange champie's position based on his height
     let idleAnimStart = setInterval(event => {idleSprite=selectedChampie.IdleAnimation(idleSprite)}, 500)  // animate champie's idle animation
     
-    //? rotate "champie" 360deg, after 0.2s modify transition duration to 0s (to disallow next animation) and remove transform, return back the 0.2s to transition duration
+    // rotate "champie" 360deg, after 0.2s modify transition duration to 0s (to disallow next animation) and remove transform, return back the 0.2s to transition duration
     champie.addEventListener("click", event => {
         champie.style.transform = "rotateY(360deg)"
         setTimeout(event = () => {champie.style.transitionDuration = "0s"; champie.style.removeProperty("transform")}, 200)
@@ -92,8 +91,5 @@ function Start() {
 //? test attack
 document.addEventListener("keydown", event => {
     key = event.keyCode
-    if (key == `69`) {
-        x = 2
-        selectedChampie.Attack(x)
-    }
+    if (key == `69`) selectedChampie.Attack()
 })
