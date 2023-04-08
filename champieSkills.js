a7823200fbba3c78240760489b55c349ceff18ad
Sheezek <pkwiktor2004@gmail.com>
2023-04-09 00:47:45 +0200

    aleksgej

--- champieSkills.js
+++ champieSkills.js
@@ -1,67 +1,227 @@
 //TODO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MISCELLANEOUS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 //? rearrange champie's position on scene (creates margin-top to put the object on the ground)
 function ChangePosition() {
-    document.querySelector("#champie_box").style.marginTop = `${document.querySelector("#background").clientHeight - champie.clientHeight}px`
+    document.querySelector("#champie_box").style.bottom = `0`
+    document.querySelector("#champie_box").style.marginBottom = `39vh`
 }
 
 
 //TODO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~REK'SAI~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let xersaiFury = false, times=-1
 
 //! --------------------------------------ATTACK
 reksaiChampie.Attack = function () {  
-    reksaiAttack.play()  // play the rek'sai's attack audio
-    pause = true
+    defend = false
+    if(!xersaiFury){
+        reksaiAttack.play()  // play the rek'sai's attack audio
+        pause = true
+        times++
 
-    // rek'sai animations during attack
-    champie.src = "Champies/Rek'Sai/Sprite_readjusted - Prepare1.png"
-    setTimeout(event => {
-        champie.src = "Champies/Rek'Sai/Sprite_readjusted - Prepare2.png"
+        // rek'sai animations during attack
+        champie.src = "Champies/Rek'Sai/Sprite_readjusted - Prepare1.png"
         setTimeout(event => {
-            champie.src = "Champies/Rek'Sai/Sprite_readjusted - Prepare3.png"
+            champie.src = "Champies/Rek'Sai/Sprite_readjusted - Prepare2.png"
             setTimeout(event => {
-                champie.src = "Champies/Rek'Sai/Sprite_readjusted - Attack.png"
+                champie.src = "Champies/Rek'Sai/Sprite_readjusted - Prepare3.png"
                 setTimeout(event => {
-                    champie.src = "Champies/Rek'Sai/Sprite_readjusted - Prepare1.png"
-                    setTimeout(event => {champie.src = "Champies/Rek'Sai/Sprite_readjusted - LieDown.png"}, 50)
-                }, 700)
+                    champie.src = "Champies/Rek'Sai/Sprite_readjusted - Attack.png"
+                    setTimeout(event => {
+                        champie.src = "Champies/Rek'Sai/Sprite_readjusted - Prepare1.png"
+                        setTimeout(event => {
+                            champie.src = "Champies/Rek'Sai/Sprite - LieDown.png"
+                            dummyEnemy.Injure()
+                        }, 50)
+                    }, 700)
+                }, 250)
             }, 250)
         }, 250)
-    }, 250)
 
-    x = 2
-    function AttackP1() {                                   //? PHASE 1
-        setTimeout(event = () => {  // move with a little delay (time)
-            champie.style.left = `${-x}px`  // every iteration decrease x to move further to the left   
+        x = 2
+        function AttackP1() {                                   //? PHASE 1
+            setTimeout(event = () => {  // move with a little delay (time)
+                champie.style.left = `${-x}px`  // every iteration decrease x to move further to the left   
     
-            x++  // lower = slower | higher = faster
-            if(x%10 == 0) time += 2  // slow down the backwards charge
-            if(x <= 80) AttackP1()  // loop the same phase to iterate the movement
-            else {x=3; time=1; x_old=x; AttackP2()}  // if x>90 then start new phase and adds this.x to the new phase 
-        }, time)
-    }
+                x++  // lower = slower | higher = faster
+                if(x%10 == 0) time += 2  // slow down the backwards charge
+                if(x <= 80) AttackP1()  // loop the same phase to iterate the movement
+                else {
+                    // if x>90 then start new phase and adds this.x to the new phase 
+                    x=3 
+                    time=1
+                    x_old=x
 
-    function AttackP2() {                                   //? PHASE 2
-        setTimeout(event = () => {  // move with a little delay (time)
-            y = Math.sin(x/150)*50  // every iteration create new y value based on function    
+                    AttackP2()
+                    document.getElementsByClassName("semi_bar")[times].style.backgroundColor = "rgb(231, 29, 73)"
+                }
+            }, time)
+        }
+
+        function AttackP2() {                                   //? PHASE 2
+            setTimeout(event = () => {  // move with a little delay (time)
+                y = Math.sin(x/150)*50  // every iteration create new y value based on sin function    
     
-            champie.style.left = `${x}px`  // move champie
-            champie.style.bottom = `${y}px`
+                champie.style.left = `${x}px`  // move champie
+                champie.style.bottom = `${y}px`
     
-            x += 3.5  // lower = slower | higher = faster
-            if(x <= 470) AttackP2()  // loop the same phase to iterate the movement
-            else setTimeout(event = () => {  // after x time resets the champie's position
-                champie.style.removeProperty("left")
-                champie.style.removeProperty("bottom")
-                champie.src = `Champies/${selectedChampie.name}/Sprite_readjusted - Idle${idleSprite}.png`
-                pause=false
-            }, 400)  
-        }, 1)
+                x += 3.5  // lower = slower | higher = faster
+                if(x <= 470) AttackP2()  // loop the same phase to iterate the movement
+                else setTimeout(event = () => {  // after x time resets the champie's position
+                    champie.style.removeProperty("left")
+                    champie.style.removeProperty("bottom")
+                    champie.src = `Champies/${selectedChampie.name}/${selectedChampie.name} - Idle${idleSprite}1.png`
+
+                    if(times == 2){
+                        times = -1
+                        xersaiFury = true
+                    }
+                    pause=false
+                }, 400)  
+            }, 1)
+        }
+        AttackP1()  //? first function call
+        }
+    else {  //? if all top bars are fully loaded, make the next attack empowered
+        pause = true
+        reksaiFuryAttack.play()
+
+        setTimeout(event => {champie.src = "Champies/Rek'Sai/Sprite - FuryAttack1.png"}, 100)
+        setTimeout(event => {
+            champie.src = "Champies/Rek'Sai/Sprite - FuryAttack2.png"
+            setTimeout(event => {
+                champie.src = "Champies/Rek'Sai/Sprite - FuryAttack3.png"
+                setTimeout(event => {
+                    champie.src = "Champies/Rek'Sai/Sprite - FuryAttack5.png"
+                    setTimeout(event => {
+                        champie.src = "Champies/Rek'Sai/Sprite - FuryAttack6.png"
+                        setTimeout(event => {
+                            champie.src = "Champies/Rek'Sai/Sprite - FuryAttack7.png"
+                            dummyEnemy.Injure()
+                            setTimeout(event => {champie.src = "Champies/Rek'Sai/Sprite - FuryAttack6_NoFury.png"}, 450)
+                        }, 700)
+                    }, 100)
+                }, 200)
+            }, 500)
+        }, 600)
+
+        // reset the top bars
+        setTimeout(event => {
+            document.querySelectorAll(".semi_bar").forEach(semi_bar => {
+                semi_bar.style.backgroundColor = "rgb(44, 36, 36)"
+            })
+            pause = false
+            xersaiFury = false
+        }, 3400)
     }
-    AttackP1()  //? first function call
 }
 
 //! --------------------------------------BLOCK
 reksaiChampie.Block = function () {  
+    pause = true
 
+    setTimeout(event => {
+        champie.src = "Champies/Rek'Sai/Sprite - FuryAttack1.png"
+        setTimeout(event => {
+            champie.src = "Champies/Rek'Sai/Sprite - FuryAttack2.png"
+            setTimeout(event => {
+                defend = true
+
+                let element = document.createElement("div")
+                element.id = "poise_panel"
+                element.innerHTML = `<img id="poise_meter" src="Interface/Poise_Meter.png">`
+
+                document.getElementById("container").appendChild(element)
+
+                let poise_meterInside = document.createElement("div")
+                poise_meterInside.id = "poise_meterInside"
+
+                document.getElementById("poise_panel").innerHTML += `<div><div id="poise_meterInside"></div></div>`
+
+
+                element = document.createElement("div")
+                element.id = "hitbox"
+                element.style.marginLeft = "40vw"
+                element.style.bottom = "57vh"
+                element.innerHTML = `<img id="hitbox_img" src="Interface/Hitbox.png" style="transform: rotate(45deg) scale(1.3)" style="translate: all 0.1s ease-in-out">`
+
+                document.getElementById("container").appendChild(element)
+
+                console.log(document.getElementById("hitbox").offsetTop)
+                console.log(document.getElementById("hitbox").offsetLeft)
+                hitboxTop = element.offsetTop
+                hitboxLeft = element.offsetLeft
+            }, 100)
+        }, 250)
+    }, 300)
+}
+
+//! --------------------------------------SPECIAL ATTACK
+reksaiChampie.Special = function () {
+    pause = true; defend = false; special = true
+
+    setTimeout(event => {
+        document.getElementById("hitbox").remove()
+        champie.src = "Champies/Rek'Sai/Sprite - FuryAttack1.png"
+        setTimeout(event => {
+            champie.src = "Champies/Rek'Sai/Sprite - SpecialAttack1.png"
+            document.getElementById("champie_box").innerHTML += `<img id="burrow">`
+            setTimeout(event => {
+                champie.style.opacity = "0%"
+                document.getElementById("burrow").src = `Champies/Rek'Sai/Sprite - SpecialAttack2.png`
+
+                reksaiBurrow.play()
+                setTimeout(event => {
+                    document.getElementById("burrow").src = "Champies/Rek'Sai/Sprite - SpecialAttack3.png"
+                    champie.style.marginLeft = "36vw"
+                    setTimeout(event => {
+                        champie.style.opacity = "100%"
+                        champie.style.zIndex = "1"
+                        document.getElementById("champie_box").innerHTML += `<img id="burrow_duplicate" src="Champies/Rek'Sai/Sprite - SpecialAttack3.png">`
+                        champie.src = "Champies/Rek'Sai/Sprite - SpecialAttack4.png"
+
+                        let x = 1
+                        specialAttack = setInterval(event => {
+                            if(x >= 250) {
+                                clearInterval(specialAttack)
+                                document.getElementById("big_black").style.zIndex = "1"
+                                document.getElementById("big_black").style.opacity = "100%"
+
+                                setTimeout(event => {
+                                    document.getElementById("champie_box").innerHTML += `<img id="slice" style="transition: all 0.2s ease-in-out; z-index: 2;" src="Champies/Rek'Sai/Sprite - Slice1.png">`
+                                    reksaiSlice.play()
+                                    setTimeout(event => {
+                                        document.getElementById("slice").src = "Champies/Rek'Sai/Sprite - Slice2.png"
+                                        setTimeout(event => {
+                                            document.getElementById("slice").src = "Champies/Rek'Sai/Sprite - Slice3.png"
+                                            setTimeout(event => document.getElementById("slice").style.opacity = "0%", 300)
+                                            setTimeout(event => {pause = false; special = false}, 600)
+                                            setTimeout(event => {
+                                                champie.style.removeProperty("z-index")
+                                                champie.style.removeProperty("margin-left")
+                                                champie.style.removeProperty("bottom")
+
+                                                document.getElementById("enemy").style.removeProperty("bottom")
+
+                                                document.getElementById("burrow").remove()
+                                                document.getElementById("burrow_duplicate").remove()
+                                                document.getElementById("slice").remove()
+
+                                                document.getElementById("big_black").style.opacity = "0%"
+                                                document.getElementById("big_black").style.zIndex = "-1"
+                                            }, 750)
+                                        }, 50)
+                                    }, 50)
+                                }, 600)
+                            }
+                            console.log(x)
+                            champie.style.bottom = `${Math.log10(x)*90}px`
+                            document.getElementById("enemy").style.bottom = `${Math.log10(x)*90}px`
+                            x++
+                        }, 5)
+
+                        reksaiUlt.play()
+                    }, 1500)
+                }, 100)
+            }, 250)
+        }, 200)
+    }, 200)
 }
--- rhythm.js
+++ rhythm.js
@@ -1,45 +1,218 @@
 //TODO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~RHYTHM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-let tick = 0, i = 0, patternTemp = [], real=true, idleSprite = 1
-    patternAttack = ["Q", "Q", "W", "Q"],
-    patternBlock = ["E", "E", "Q", "W"]
+let tick = 0, i = 0, patternTemp = [], idleSprite = 2, countPopup = 0, popupActive = true, countDefendPopups = 0, listDefendPopups = [], hitboxLeft = 0, hitboxTop = 0,
+    letterTop = 0, letterLeft = 0, distanceY = 0, distanceX = 0, special = false, poiseWidth = 0,
+    popupClass = document.getElementsByClassName("popup"),
+    patternAttack = ["Q", "Q", "W"],  // patterns
+    patternBlock = ["E", "W", "E"],
+    patternMove = ["W", "W", "W"]
 
-//? check and call out a score based on user's correct input between ticks
-function CheckTick () {
-    if(tick >= 0.9 || tick <= 0.2) console.log("PERFECT!!!")
-    else if(tick >= 0.8 || tick <= 0.4) console.log("Good!")
-    else if(tick >= 0.4 && tick <= 0.8) console.log("Okay")
+//? vanish all popups if no input
+function VanishPopup () {
+    countPopup = 3
+    i = 0
+    popupActive = false  // disables adding more popups in ModifyPopup and keys to the patterns in RhythmStart
+    ResetPopup()  // call ResetPopup()
+
+    patternTemp = []  // resets all pressed keys in the pattern
+}
+let vanishTimeout = setTimeout(event => {VanishPopup()}, 1000)
+clearTimeout(vanishTimeout)
+
+//? if popupActive, then move a single popup, change it's opacity, resize it, 
+//? add tick score and add the pressed letter
+function ModifyPopup (letter, text) {
+    if(popupActive) {
+        popupClass[countPopup].style.transition = "all 0.5s ease-out, scale 0.2s ease-in-out"
+        popupClass[countPopup].style.transform = `translate(${Math.floor(Math.random() * 1400) + 250}px, ${Math.floor(Math.random()*300)+10}px)`
+        popupClass[countPopup].style.transform += `rotate(${Math.floor(Math.random()*56)-25}deg)`
+        setTimeout(event => {popupClass[countPopup].style.transform += `scale(1.2)`}, 200)
+        popupClass[countPopup].style.opacity = "1"
+
+        document.getElementsByClassName("popup")[countPopup].querySelector("h2").innerHTML = text
+        document.getElementsByClassName("popup")[countPopup].querySelector("h3").innerHTML = letter
+        countPopup++
+
+        if(text == "PERFECT!!!"){  // if the tick score is PERFECT!!!, then modify body and after 0.25s revert the changes
+            body.style.transition = `all 0.15s`
+            body.style.transform = `scale(1.03)`
+            body.style.transform += `rotate(${Math.random()*5-2}deg)`
+
+            setTimeout (event => {body.style.transform = `scale(1)`}, 250)
+        }
+    }
 }
 
-//? call it to start the whole rhythm
+//? if quantity of popups == 3, then disable the "if" above, 
+//? after 1.5s for each popup change its opacity to 0% and after 0.5s change its h's innerHTMLs to ""
+function ResetPopup () {
+    document.querySelectorAll(".popup").forEach(popup => {
+        popup.style.opacity = "0%"
+        popup.style.removeProperty("transform")
+        popup.style.transform = `translate(${Math.floor(Math.random() * 1400) + 250}px, ${Math.floor(Math.random()*300)+10}px)`
+
+        setTimeout(event => {  // after 0.5s reset h2 and h3 in every popup to empty
+            popup.querySelector("h2").innerHTML = ""
+            popup.querySelector("h3").innerHTML = ""
+        }, 500)
+    })
+    countPopup = 0  // reset countPopup count
+    setTimeout(event => {popupActive = true}, 500)  // after 0.5s activate the ModifyPopup() 
+} 
+
+//? check and call out a score based on user's correct input between ticks
+function CheckTick (letter) {
+    if(tick >= 0.9 || tick <= 0.2){
+        // call ModifyPopup, ResetPopup
+        ModifyPopup(letter, "PERFECT!!!")
+        if(countPopup == 3){
+            popupActive = false  // disables adding more popups in ModifyPopup and keys to the patterns in RhythmStart
+            setTimeout(event => {ResetPopup()}, 1500)
+        } 
+
+        console.log("PERFECT!!!")
+    }
+    else if(tick >= 0.8 || tick <= 0.4){
+        // call ModifyPopup, ResetPopup
+        ModifyPopup(letter, "Good!")
+        if(countPopup == 3){
+            popupActive = false  // disables adding more popups in ModifyPopup and keys to the patterns in RhythmStart
+            setTimeout(event => {ResetPopup()}, 1500)
+        } 
+
+        console.log("Good!")
+    } 
+    else if(tick >= 0.4 && tick <= 0.8){
+        // call ModifyPopup, ResetPopup
+        ModifyPopup(letter, "Okay")
+        if(countPopup == 3){
+            popupActive = false  // disables adding more popups in ModifyPopup and keys to the patterns in RhythmStart
+            setTimeout(event => {ResetPopup()}, 1500)  // after 1.5s call ResetPopup
+        } 
+
+        console.log("Okay")
+    } 
+}
+
+//? function to add another popup during defence
+function DefendPopupAdd(letter) {
+    rank = "Missed"
+    poise = document.getElementById("poise_meterInside")
+    hitbox.currentTime = 0
+    hitbox.play()
+
+    if (document.getElementsByClassName("attack_popup")[0] != null){    // checks if the popup exist, to not create an error
+        letterTop = document.getElementsByClassName("attack_popup")[0].offsetTop    // calculates distances between objects `hitbox` and `attack_popup`
+        letterLeft = document.getElementsByClassName("attack_popup")[0].offsetLeft
+        distanceY = hitboxTop - letterTop
+        distanceX = letterLeft - hitboxLeft
+
+        // based on distances give user his rank - [DISTANCE_Y ; DISTANCE_Y-30; DISTANCE_X+30 ; DISTANCE_X]
+        if (letter != document.getElementsByClassName("attack_popup")[0].innerHTML){
+            rank = "Wrong Letter!"
+        }
+        else if (distanceY < 35 && distanceY > -65 && distanceX < 85 && distanceX > -35){
+            rank = "PERFECT!!!"
+            document.getElementById("hitbox_img").style.transition = "all 0.1s ease-in-out" 
+            document.getElementById("hitbox_img").style.transform = "scale(2) rotate(45deg)"
+        
+            if (rank === "PERFECT!!!" && poiseWidth < 200) {
+                poiseWidth += 20
+                poise.style.width = poiseWidth + "px"
+            }
+
+            setTimeout(event => document.getElementById("hitbox_img").style.transform = "scale(1.3) rotate(45deg)", 150)
+        }
+        else if (distanceY < 55 && distanceY > -85 && distanceX < 115 && distanceX > -60){
+            rank = "Good!"
+        }
+        else rank = "Okay"
+    }
+
+    countDefendPopups++
+    listDefendPopups.push(new DefendPopup(countDefendPopups, letter, rank))   // creates new object of class DefendPopup to the list
+    listDefendPopups[countDefendPopups-1].Begin()   // calls the object's method Begind()
+
+    console.log(`${letter} with rank ${rank}`)
+}
+
+//? function to add another popup during attack
+function AttackPopupAdd(letter) {
+    patternTemp[i] = letter   // adds another letter to the temporary pattern var
+    i++
+    CheckTick(letter)  // call CheckTick() function
+
+    switch (letter){  // depending on the acquired letter - play an appropiate audio
+        case "Q":
+            reksaiQ.currentTime = 0
+            reksaiQ.play()
+            break
+        case "W":
+            reksaiW.currentTime = 0
+            reksaiW.play()
+            break
+        case "E":
+            reksaiE.currentTime = 0
+            reksaiE.play()
+            break
+    }
+    
+    clearTimeout(vanishTimeout)
+    vanishTimeout = setTimeout(event => {VanishPopup()}, 2000)
+}
+
+//? call it to start the whole rhythm  <----------------------------------------------------------------------------------------
 function RhythmStart() {
+    // for each popup give them a starting position 
+    document.querySelectorAll(".popup").forEach(popup => popup.style.transform = `translate(${Math.floor(Math.random() * 1400) + 250}px, ${Math.floor(Math.random() * 300) + 10}px)`)
+
     // create an interval, where every 0.1s tick goes up to max value of 1
     let rhythm = setInterval(event => {
         tick += 0.1
         tick = parseFloat(tick.toFixed(1))
-        if(tick >= 1) {
+        if(tick >= 1) {  // ticks reset every 10 iterations
             tick = 0
             idleSprite = selectedChampie.IdleAnimation(idleSprite)  // animate champie's idle animation (synchro with the tickrate)
         }
-        console.log(tick)
+        //console.log(tick)
     }, 50)
 
-    // add to the whole window a listener listening to keys pressed on the keyboard
+    // add to the whole document a listener listening to keys pressed on the keyboard
     document.addEventListener("keydown", event => {
-        key = event.keyCode  // get pressed key's data
+        if(popupActive){
+            key = event.keyCode  // get pressed key's data
 
-        // check if the key clicked is Q -> W -> E -> R
-        if (key == `81`) {patternTemp[i] = "Q"; i++; CheckTick()} 
-        else if (key == `87`) {patternTemp[i] = "W"; i++; CheckTick()}
-        else if (key == `69`) {patternTemp[i] = "E"; i++; CheckTick()} 
-        else if (key == `82`) {patternTemp[i] = "R"; i++; CheckTick()}
+            if (i == 0 && !defend) vanishTimeout = setTimeout(event => {VanishPopup()}, 2000)
 
-        // if pattern has already 4 letters (keys) in, then check if it is similar to any of patterns
-        if(patternTemp.length == 4) {
-            for(j=0; j <=3; j++){
-                if(patternTemp[j] != patternAttack[j]) {patternTemp = []; i = 0; break}
-                else if(j == 3) {patternTemp = []; i = 0; selectedChampie.Attack()}
+            // mechanism of popups during defence/attack sequences
+            if (defend){        // if the key "Q", "W" or "E" is pressed, call function DefendPopupAdd(), else delete all defend_popups' content, reset variables and stop champie def stance
+                if (key == `81`) DefendPopupAdd("Q")
+                else if (key == `87`) DefendPopupAdd("W")
+                else if (key == `69`) DefendPopupAdd("E")
+                else if (key == `82`) { 
+                    listDefendPopups.forEach(defendPopup => document.getElementById(`defend_popup${defendPopup.id}`).remove())
+                    countDefendPopups = 0; listDefendPopups = []
+                    selectedChampie.DefendStop(); 
+                }
+                else if (key == '83') reksaiChampie.Special()
+            }
+            else if (!special) {              // check if the key pressed is Q -> W -> E - if it is, then call function AttackPopupAdd()
+                if (key == `81`) AttackPopupAdd("Q")
+                else if (key == `87`) AttackPopupAdd("W")
+                else if (key == `69`) AttackPopupAdd("E") 
+                console.log(patternTemp[i])
+            }
+
+            // if pattern has already 4 letters (keys) in, then check if it is similar to any of patterns
+            if(patternTemp.length == 3 && !defend) {
+                clearTimeout(vanishTimeout)
+                for(j=0; j <= 2; j++){
+                    if(patternTemp[j] != patternAttack[j] && patternTemp[j] != patternBlock[j]) {patternTemp = []; i = 0; break}  // if any of the values from the table are not the same, clear the table, reset i and break the loop
+                    else if(j == 2 && patternTemp[0] == "Q") {patternTemp = []; i = 0; selectedChampie.Attack()}
+                    else if(j == 2 && patternTemp[0] == "E") {patternTemp = []; i = 0; selectedChampie.Block()}
+                }
             }
         }
+        
     })
 }
 
